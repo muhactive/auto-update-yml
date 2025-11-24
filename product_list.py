32274881e82fbbd44c1554813b07315f3913ca66
Alex Mukha <muhactive@gmail.com>
2025-11-24 15:41:28 +0200

    auto update yml

--- product_list.py
+++ product_list.py
@@ -1,19 +1,19 @@
 PRODUCT_LIST = [
-        ("https://lugi.com.ua/poverbank-5000-mah-usb-c-usb-a/", 1),
-        ("https://lugi.com.ua/poverbank-10000-mah-z-bezdrotovoyu-zaryadkoyu-usb-s-usb-a-microusb-apple-lightning-3-shnury-v-komplekti/", 1),
-        ("https://lugi.com.ua/kuhonnyj-kombajn-sokany-sm-5011-7-600-vt/", 2),
-        ("https://lugi.com.ua/kuhonnyj-kombajn-sokany-sm-5011-8-bagatofunkcionalnyj-chornyj/", 2),
-        ("https://lugi.com.ua/kuhonnyj-kombajn-4-v-1-sokany-sk-03029-z-sokovyzhymalkoyu/", 2),
-        ("https://lugi.com.ua/m-yasorubka-elektrychna-sokany-sk-312-z-nasadkamy/", 2),
-        ("https://lugi.com.ua/m-yasorubka-elektrychna-sokany-sk-087-z-nasadkamy/", 2),
-        ("https://lugi.com.ua/m-yasorubka-elektrychna-sokany-sk-092-profesijna-stal/", 2),
-        ("https://lugi.com.ua/vagy-kuhonni-elektronni-sokany-sk-1204-z-chasheyu-hrom/", 2),
-        ("https://lugi.com.ua/vagy-kuhonni-elektronni-sokany-sk-19027-do-5-kg-z-ploskoyu-platformoyu-chornyj/", 2),
-        ("https://lugi.com.ua/girlyanda-konskij-hvost-600-led-20-nitej-dlina-3-metra-belyj/", 3),
-        ("https://lugi.com.ua/girlyanda-nit-rosa-hvojnaya-lapa-3000-led-100-metrov-zelenaya-nit-sinij/", 3),
-        ("https://lugi.com.ua/girlyanda-nit-rosa-hvojnaya-lapa-1500-led-50-metrov-zelenaya-nit-multikolor/", 3),
-        ("https://lugi.com.ua/girlyanda-konskij-hvost-600-led-20-nitej-dlina-3-metra-zheltyj/", 3),
-        ("https://lugi.com.ua/girlyanda-shtora-hvojnaya-lapa-svetodiodnaya-400-led-3h2-m-zhovtyj/", 3)
+        ("https://lugi.com.ua/poverbank-5000-mah-usb-c-usb-a/", 148986728),
+        ("https://lugi.com.ua/poverbank-10000-mah-z-bezdrotovoyu-zaryadkoyu-usb-s-usb-a-microusb-apple-lightning-3-shnury-v-komplekti/", 148986728),
+        ("https://lugi.com.ua/kuhonnyj-kombajn-sokany-sm-5011-7-600-vt/", 148986746),
+        ("https://lugi.com.ua/kuhonnyj-kombajn-sokany-sm-5011-8-bagatofunkcionalnyj-chornyj/", 148986746),
+        ("https://lugi.com.ua/kuhonnyj-kombajn-4-v-1-sokany-sk-03029-z-sokovyzhymalkoyu/", 148986746),
+        ("https://lugi.com.ua/m-yasorubka-elektrychna-sokany-sk-312-z-nasadkamy/", 148986746),
+        ("https://lugi.com.ua/m-yasorubka-elektrychna-sokany-sk-087-z-nasadkamy/", 148986746),
+        ("https://lugi.com.ua/m-yasorubka-elektrychna-sokany-sk-092-profesijna-stal/", 148986746),
+        ("https://lugi.com.ua/vagy-kuhonni-elektronni-sokany-sk-1204-z-chasheyu-hrom/", 148986746),
+        ("https://lugi.com.ua/vagy-kuhonni-elektronni-sokany-sk-19027-do-5-kg-z-ploskoyu-platformoyu-chornyj/", 148986746),
+        ("https://lugi.com.ua/girlyanda-konskij-hvost-600-led-20-nitej-dlina-3-metra-belyj/", 148986747),
+        ("https://lugi.com.ua/girlyanda-nit-rosa-hvojnaya-lapa-3000-led-100-metrov-zelenaya-nit-sinij/", 3148986747),
+        ("https://lugi.com.ua/girlyanda-nit-rosa-hvojnaya-lapa-1500-led-50-metrov-zelenaya-nit-multikolor/", 148986747),
+        ("https://lugi.com.ua/girlyanda-konskij-hvost-600-led-20-nitej-dlina-3-metra-zheltyj/", 148986747),
+        ("https://lugi.com.ua/girlyanda-shtora-hvojnaya-lapa-svetodiodnaya-400-led-3h2-m-zhovtyj/", 148986747)
 ]
 
 CATEGORY_ID = {
